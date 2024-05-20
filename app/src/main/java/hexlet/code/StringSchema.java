a78f404de7c0c11b9c1e0bf104597f131b208545
ungrome <ungarov.roman@gmail.com>
2024-05-20 23:39:39 +0300

    add Validator.java & StringSchema.java

---
+++ app/src/main/java/hexlet/code/StringSchema.java
@@ -0,0 +1,15 @@
+package hexlet.code;
+
+public class StringSchema {
+    public StringSchema required(){
+        return this;
+    }
+
+    public StringSchema minLength(){
+        return this;
+    }
+
+    public StringSchema contains(){
+        return this;
+    }
+}
---
+++ app/src/main/java/hexlet/code/Validator.java
@@ -0,0 +1,8 @@
+package hexlet.code;
+
+public class Validator {
+    public StringSchema string() {
+        StringSchema stringSchema = new StringSchema();
+        return stringSchema;
+    }
+}
