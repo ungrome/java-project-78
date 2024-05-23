8f6716302855ed0e13d56c62a9610bd0bb309ffe
ungrome <ungarov.roman@gmail.com>
2024-05-24 01:18:01 +0300

    add StringSchema.java & StringSchemaTest.java (step2)

--- app/src/main/java/hexlet/code/StringSchema.java
+++ app/src/main/java/hexlet/code/StringSchema.java
@@ -1,7 +1,5 @@
 package hexlet.code;
 
-import com.sun.jdi.ObjectReference;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Predicate;
@@ -10,18 +8,18 @@ public class StringSchema {
 
     private final Map<String, Predicate<Object>> checkResults = new HashMap<>();
     public StringSchema required() {
-        Predicate <Object> check = (obj) -> obj instanceof String str && !str.isBlank();
+        Predicate<Object> check = (obj) -> obj instanceof String str && !str.isBlank();
         checkResults.put("required", check);
         return this;
     }
     public StringSchema minLength(Integer minLen) {
-        Predicate <Object> check = (obj) -> obj instanceof String str && str.length() >= minLen;
+        Predicate<Object> check = (obj) -> obj instanceof String str && str.length() >= minLen;
         checkResults.put("minLength", check);
         return this;
     }
 
     public StringSchema contains(String str) {
-        Predicate <Object> check = (obj) -> obj instanceof String s && s.contains(str);
+        Predicate<Object> check = (obj) -> obj instanceof String s && s.contains(str);
         checkResults.put("contains", check);
         return this;
     }
